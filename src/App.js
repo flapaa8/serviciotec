6d8b219f5d0c29e8af01c820129cf223d9cc8c65
flapaa8 <yoquieroayudaroficial@gmail.com>
2021-01-30 14:01:07 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import data from './data'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="grid-container">
+        <header class="row">
+            <div>
+                <a className="brand" href="/">Servicio Tec</a>
+            </div>
+            <div>
+                <a href="/cart">Carrito</a>
+                <a href="/signin">Iniciar sesion</a>
+            </div>
+        </header>
+        <main>
+            <div class="row center">
+              {data.products.map((product) => (
+                <div key={product._id} className="card">
+                    <a href={`/product/${product._id}`}>
+                        <img class="medium" src={product.image} alt={product.name}/>
+                    </a>
+                    <div class="card-body">
+                    <a href={`/product/${product._id}`}>
+                            <h2>{product.name} </h2>
+                        </a>
+                        <div class="rating">
+                            <span>
+                                <i class="fa fa-star"></i>
+                            </span>
+                            <span>
+                                <i class="fa fa-star"></i>
+                            </span>
+                            <span>
+                                <i class="fa fa-star"></i>
+                            </span>
+                            <span>
+                                <i class="fa fa-star"></i>
+                            </span>
+                            <span>
+                                <i class="fa fa-star"></i>
+                            </span>
+                        </div>
+                        <div class="price">${product.price}</div>
+                    </div>
+                </div>
+                ))}
+            </div>
+        </main>
+        <footer class="row center">
+            @  2021 Servicio Tec  -  Todos Los Derechos Reservados        </footer>
     </div>
   );
 }
